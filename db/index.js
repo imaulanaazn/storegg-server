c947161c880d2a721c0526dab8eea0dc594da23b
Irham Maulana <i.maulana.azn@gmail.com>
2023-06-18 09:34:55 +0700

    change mongoose.connection 2nd param

--- db/index.js
+++ db/index.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 const { urlDb } = require('../config')
 
-mongoose.connect(urlDb, {
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-  useNewUrlParser: true 
-})
+mongoose.connect(urlDb, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db =  mongoose.connection
 
